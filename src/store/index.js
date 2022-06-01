2514da729864acbc2624531965e397a8539f4447
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-06-01 12:57:22 +0100

    vuex

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import todos from './modules/todos'
+
+
+// load vuex 
+Vue.use(Vuex);
+
+// create store 
+export default new Vuex.Store({
+    modules:{
+        todos
+    },
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    },
+});
